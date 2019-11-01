4bf0d39878f5a3376862f198021e023bff675543
Ashutosh <Ashutosh@company.com>
2019-11-02 03:12:10 +0530

    Second_commit

--- MatriNome/users/urls.py
+++ MatriNome/users/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('profile/<int:pk>/', user_views.ProfileDetailView, name='profile_view'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('contact/<int:pk>/', views.contact, name='profile_contact'),
+    path('smash/<int:pk>/', views.contact, name='profile_contact'),
+    path('pass/<int:pk>/', views.delete_contact, name='profile_contact_delete'),
 ]
